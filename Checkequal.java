500a354ae6042681116f60a0429a9f282fe364b0
SManasa105 <smanasa105@gmail.com>
2025-11-16 14:29:22 +0530

    Operators

---
+++ Checkequal.java
@@ -0,0 +1,18 @@
+public class Checkequal {
+    
+
+    public static void main(String[] args) {
+
+        int num1 = 10;
+        int num2 = 20;
+
+        if (num1 == num2) {
+            System.out.println("Both numbers are equal");
+        } else {
+            System.out.println("Numbers are not equal");
+        }
+    }
+}
+
+    
+
---
+++ operators.java
@@ -0,0 +1,23 @@
+public class operators {
+
+    static void showOperators() {
+        int a = 5;
+        int b = 10;
+
+        System.out.println("Original a: " + a);
+        System.out.println("Original b: " + b);
+
+        a++;  
+        b--;  
+
+        System.out.println("After increment (a++): " + a);
+        System.out.println("After decrement (b--): " + b);
+    }
+
+    public static void main(String[] args) {
+        showOperators(); 
+    }
+}
+
+    
+
